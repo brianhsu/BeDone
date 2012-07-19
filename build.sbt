01facde68cc8ae183576bf203a0f91f9794581e8
Brian Hsu <brianhsu.hsu@gmail.com>
2012-07-19 10:05:47 +0800

    Add basic lift dependency.

--- build.sbt
+++ build.sbt
@@ -10,7 +10,8 @@ scalaVersion := "2.9.2"     // 我們要使用的 Scala 版本
 
 libraryDependencies ++= Seq(
     "org.eclipse.jetty" % "jetty-webapp" % "8.0.1.v20110908" % "container",
-    "javax.servlet" % "servlet-api" % "2.5" % "provided"
+    "javax.servlet" % "servlet-api" % "2.5" % "provided",
+    "net.liftweb" % "lift-webkit_2.9.1" % "2.4" % "compile"
 )
 
 
---
+++ src/main/scala/bootstrap/Boot.scala
@@ -0,0 +1,21 @@
+// Filename: BeDone/src/main/scala/bootstrap/Boot.scala
+
+package bootstrap.liftweb
+
+import net.liftweb.http.LiftRules
+import net.liftweb.http.Html5Properties
+import net.liftweb.http.Req
+
+class Boot {
+    def boot {
+
+        // 我們的程式碼放在 org.bedone 這個 package 中
+        LiftRules.addToPackages("org.bedone")
+
+        // 使用 HTML 5 做為模版
+        LiftRules.htmlProperties.default.set { r: Req => 
+            new Html5Properties(r.userAgent)
+        }
+    }
+}
+
