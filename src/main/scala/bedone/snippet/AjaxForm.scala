c774b544901330b4a0655dcbef59d642e7c02fa1
Brian Hsu <brianhsu.hsu@gmail.com>
2012-07-21 08:47:17 +0800

    Add notice area.

--- src/main/scala/bedone/snippet/AjaxForm.scala
+++ src/main/scala/bedone/snippet/AjaxForm.scala
@@ -2,85 +2,23 @@ package org.bedone.snippet
 
 import org.bedone.model._
 
-import net.liftweb.common.Box
 import net.liftweb.util.Helpers._
 import net.liftweb.util.FieldError
 
 import net.liftweb.record.field._
-import net.liftweb.record.Field
 import net.liftweb.record.BaseField
-import net.liftweb.record.MetaRecord
 import net.liftweb.record.Record
-
-import net.liftweb.util.FieldIdentifier
 
 import net.liftweb.http.SHtml
 import net.liftweb.http.SHtml.ElemAttr
 import net.liftweb.http.Templates
 import net.liftweb.http.js.JsCmds._
 import net.liftweb.http.js.JsCmd
-import net.liftweb.http.js.jquery.JqJsCmds.JqSetHtml
 import net.liftweb.http.js.JE.JsRaw
+import net.liftweb.http.js.jquery.JqJsCmds.JqSetHtml
 
 import scala.xml.NodeSeq
 import scala.xml.Text
-
-class SignupDialog extends AjaxForm[User]
-{
-    private var confirmPassword: String = _
-
-    override protected val record = User.createRecord
-    override protected val fields = List(record.username, record.email, record.password)
-
-    def saveAndClose(): JsCmd = {
-        record.saveTheRecord() 
-        JsRaw(""" $('#signupModal').modal('hide') """)
-    }
-
-    def signup(): JsCmd = {
-
-        record.validate match {
-            case Nil if isPasswordConfirmed => saveAndClose()
-            case errors => passwordConfirmJS & this.showAllError(errors)
-        }
-    }
-
-    def isPasswordConfirmed = record.password.match_?(this.confirmPassword)
-
-    def passwordConfirmJS: JsCmd = {
-        val fieldID = "confirmPassword"
-
-         isPasswordConfirmed match {
-            case true  => removeFieldError(fieldID)
-            case false => showFieldError(fieldID, Text("密碼不一致"))
-        }
-    }
-
-    def confirmPasswordBinding() =
-    {
-        val fieldID = "confirmPassword"
-        val messageID = fieldID + "_msg"
-
-        def ajaxTest(value: String) = {
-            this.confirmPassword = value
-            passwordConfirmJS
-        }
-
-        ".control-group [id]" #> fieldID &
-        ".control-group *" #> (
-            ".control-label *" #> "Confirm Password" &
-            ".help-inline [id]" #> messageID &
-            "input" #> SHtml.textAjaxTest("", doNothing _, ajaxTest _, "type" -> "password")
-        )
-    }
-
-    override def cssBinding: List[net.liftweb.util.CssSel] = 
-        super.cssBinding :+ confirmPasswordBinding
-
-    def render = 
-        ".modal-body *" #> this.toForm &
-        ".btn-primary" #> SHtml.a(signup _, Text("註冊"))
-}
 
 abstract class AjaxForm[T <: Record[T]]
 {
