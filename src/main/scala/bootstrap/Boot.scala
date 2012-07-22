b49e086228dd21574514d180f64dad1b8559e625
Brian Hsu <brianhsu.hsu@gmail.com>
2012-07-22 12:41:45 +0800

    Add basic ajax loader.

--- src/main/scala/bootstrap/Boot.scala
+++ src/main/scala/bootstrap/Boot.scala
@@ -2,13 +2,29 @@
 
 package bootstrap.liftweb
 
+import net.liftweb.common.Full
+
 import net.liftweb.http.LiftRules
 import net.liftweb.http.Html5Properties
 import net.liftweb.http.Req
 import net.liftweb.record.field.PasswordField
 
-class Boot {
-    def boot {
+class Boot 
+{
+    def initAjaxLoader()
+    {
+        LiftRules.ajaxStart = Full(() => {
+            LiftRules.jsArtifacts.show("ajax-loader").cmd &
+            LiftRules.jsArtifacts.show("ajax-loader-signup").cmd
+        })
+        LiftRules.ajaxEnd = Full(() => {
+            LiftRules.jsArtifacts.hide("ajax-loader").cmd &
+            LiftRules.jsArtifacts.hide("ajax-loader-signup").cmd
+        })
+    }
+
+    def boot 
+    {
 
         // 我們的程式碼放在 org.bedone 這個 package 中
         LiftRules.addToPackages("org.bedone")
@@ -18,6 +34,7 @@ class Boot {
             new Html5Properties(r.userAgent)
         }
 
+        initAjaxLoader()
         PasswordField.minPasswordLength = 7
 
         DBSettings.initDB()
