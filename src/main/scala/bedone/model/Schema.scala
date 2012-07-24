365fb469fe11f08b64f801db34d11d1678772959
Brian Hsu <brianhsu.hsu@gmail.com>
2012-07-24 14:58:51 +0800

    Add StuffTopics MySQL table.

--- src/main/scala/bedone/model/Schema.scala
+++ src/main/scala/bedone/model/Schema.scala
@@ -7,12 +7,17 @@ object BeDoneSchema extends Schema {
 
     val users = table[User]("users")
     val stuffs = table[Stuff]("stuffs")
+    val stuffTopics = table[StuffTopic]("stuff_topics")
 
     // Unique and Index
     on(users) { user => declare(user.username defineAs unique, user.email defineAs unique) }
+    on(stuffTopics) { stuffTopic => declare(
+        columns(stuffTopic.stuffID, stuffTopic.topic) are unique
+    )}
 
     // Foreign Keys
     oneToManyRelation(users, stuffs).via { (user, stuff) => user.id === stuff.userID }
-
-
+    oneToManyRelation(stuffs, stuffTopics).via { 
+        (stuff, stuffTopic) => stuff.id === stuffTopic.stuffID 
+    }
 }    
--- src/main/scala/bedone/model/Stuff.scala
+++ src/main/scala/bedone/model/Stuff.scala
@@ -23,6 +23,28 @@ import net.liftweb.util.Helpers.tryo
 import java.io.StringReader
 import java.io.StringWriter
 
+object StuffTopic extends StuffTopic with MetaRecord[StuffTopic]
+class StuffTopic extends Record[StuffTopic] {
+    def meta = StuffTopic
+
+    val stuffID = new LongField(this)
+    val topic = new StringField(this, "") {
+        override def validations = valMinLen(1, "此為必填欄位")_ :: super.validations
+    }
+
+    override def saveTheRecord = inTransaction ( tryo {
+        import BeDoneSchema.stuffTopics
+
+        val oldTopics = stuffTopics.where(t => t.stuffID === stuffID and t.topic === topic)
+
+        oldTopics.toList match {
+            case Nil => stuffTopics.insert(this)
+            case xs  => this
+        }
+    })
+
+}
+
 object Stuff extends Stuff with MetaRecord[Stuff]
 {
     def findByUser(user: User): Box[List[Stuff]] = inTransaction {
@@ -40,8 +62,8 @@ class Stuff extends Record[Stuff] with KeyedRecord[Long] {
     def meta = Stuff
 
     @Column(name="id")
-    val idField = new LongField(this, 1)
-    val userID = new LongField(this, 1)
+    val idField = new LongField(this)
+    val userID = new LongField(this)
     val createTime = new DateTimeField(this)
 
     val title = new StringField(this, "") {
