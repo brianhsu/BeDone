769415aa1550f106e55b560531fca01fb9c0fe27
Brian Hsu <brianhsu.hsu@gmail.com>
2012-08-04 11:20:49 +0800

    Add basic action list.

--- src/main/scala/bedone/snippet/ScheduledAction.scala
+++ src/main/scala/bedone/snippet/ScheduledAction.scala
@@ -24,12 +24,16 @@ class ScheduledAction extends JSImplicit
     lazy val dateTimeFormatter = new SimpleDateFormat("yyyy-MM-dd hh:mm")
     lazy val dateFormatter = new SimpleDateFormat("yyyy-MM-dd")
 
+    private var currentTopic: Option[Topic] = None
+    private var currentProject: Option[Project] = None
+
     val currentUser = CurrentUser.get.get
+
     def scheduledAction = Scheduled.findByUser(currentUser).openOr(Nil)
     def todayAction = scheduledAction.filter(isToday)
-    def weekAction  = scheduledAction.filter(isThisWeek)
-    def monthAction = scheduledAction.filter(isThisMonth)
-
+    def weekAction  = scheduledAction.filter(isThisWeek).filterNot(isToday)
+    def monthAction = scheduledAction.filter(isThisMonth).filterNot(isToday)
+    def allAction = scheduledAction.filterNot(isToday)
 
     def isThisMonth(scheduled: Scheduled): Boolean =
     {
@@ -60,12 +64,114 @@ class ScheduledAction extends JSImplicit
         scheduled.startTime.is.getTime.before(todayEnd.toDate)
     }
 
+    def formatDoneTime(action: Action) = 
+    {
+        action.doneTime.is match {
+            case None => "*" #> ""
+            case Some(calendar) => ".label *" #> dateTimeFormatter.format(calendar.getTime)
+        }
+    }
+
+    def formatStartTime(scheduled: Scheduled) = 
+    {
+        ".label *" #> dateTimeFormatter.format(scheduled.startTime.is.getTime)
+    }
+
+    def showAllStuff() = 
+    {
+        this.currentTopic = None
+        this.currentProject = None
+
+        updateList() &
+        JqSetHtml("current", "全部") &
+        """$('#showAll').prop("disabled", true)""" &
+        """$('#current').attr("class", "btn btn-inverse")"""
+    }
+
+    def topicFilter(buttonID: String, topic: Topic) = 
+    {
+        this.currentProject = None
+        this.currentTopic = Some(topic)
+
+        updateList() &
+        JqSetHtml("current", topic.title.is) &
+        """$('#showAll').prop("disabled", false)""" &
+        """$('#current').attr("class", "btn btn-info")"""
+    }
+
+    def projectFilter(buttonID: String, project: Project) =
+    {
+        this.currentProject = Some(project)
+        this.currentTopic = None
+
+        updateList() &
+        JqSetHtml("current", project.title.is) &
+        """$('#showAll').prop("disabled", false)""" &
+        """$('#current').attr("class", "btn btn-success")"""
+    }
+
+    def updateList(): JsCmd =
+    {
+        Noop
+    }
+
+    def createActionRow(scheduled: Scheduled) = 
+    {
+        import TagButton.Implicit._
+
+        def template = Templates("templates-hidden" :: "scheduled" :: "item" :: Nil)
+
+        val action = scheduled.action
+        val stuff = action.stuff
+
+        val cssBinding = 
+            ".action [id]"    #> ("row" + action.idField) &
+            ".collapse [id]"  #> ("desc" + action.stuff.idField) &
+            ".title *"        #> stuff.title &
+            ".desc *"         #> stuff.descriptionHTML &
+            ".topic"          #> action.topics.map(_.viewButton(topicFilter)) &
+            ".project"        #> action.projects.map(_.viewButton(projectFilter)) &
+            ".startTime"      #> formatStartTime(scheduled) &
+            ".doneTime"       #> formatDoneTime(action)
+
+        template.map(cssBinding).openOr(<span>Template does not exists</span>)
+    }
+
+    def createActionList(intervalAction: List[Scheduled]) = 
+    {
+        val todayList = todayAction.filter(!_.action.isDone.is)
+        val intervalList = intervalAction.filter(!_.action.isDone.is)
+        val doneList = 
+            todayList.filter(_.action.isDone.is) ++ 
+            intervalList.filter(_.action.isDone.is)
+
+        (todayList, intervalList, doneList)
+    }
+
+    def updateList(tabID: String, title: String, intervalAction: List[Scheduled]): JsCmd = 
+    {
+        val (todayList, intervalList, doneList) = createActionList(intervalAction)
+
+        """$('.intervalTab').removeClass('active')""" &
+        """$('#%s').addClass('active')""".format(tabID) &
+        JqSetHtml("intervalLabel", title)  &
+        JqSetHtml("intervalList", intervalList.flatMap(createActionRow)) &
+        JqSetHtml("todayList", todayList.flatMap(createActionRow)) &
+        JqSetHtml("doneList", doneList.flatMap(createActionRow))
+    }
+
     def render = 
     {
-        println("today:" + todayAction.map(_.action.stuff.title))
-        println("thisWeek:" + weekAction.map(_.action.stuff.title))
-        println("thisMonth:" + monthAction.map(_.action.stuff.title))
-        println("all:" + scheduledAction.map(_.action.stuff.title))
-        "aaa" #> "QQQQ"
+        val (todayList, intervalList, doneList) = createActionList(weekAction)
+
+        "#thisWeekTab [onclick]" #> 
+            SHtml.onEvent(s => updateList("thisWeekTab", "本週", weekAction)) &
+        "#thisMonthTab [onclick]" #> 
+            SHtml.onEvent(s => updateList("thisMonthTab", "本月", monthAction)) &
+        "#allTab [onclick]" #> 
+            SHtml.onEvent(s => updateList("allTab", "全部", allAction)) &
+        "#todayList *"    #> todayList.flatMap(createActionRow) &
+        "#intervalList *" #> intervalList.flatMap(createActionRow) &
+        "#doneList *"     #> doneList.flatMap(createActionRow)
     }
 }
