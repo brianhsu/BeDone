04e951127891a1fbc584a05b0a8bc4ecf05f2b32
Brian Hsu <brianhsu.hsu@gmail.com>
2012-09-04 14:27:02 +0800

    Add basic row.

---
+++ src/main/scala/bedone/snippet/ContactTable.scala
@@ -0,0 +1,32 @@
+package org.bedone.snippet
+
+import org.bedone.model._
+import org.bedone.lib._
+
+import net.liftweb.util.Helpers._
+
+import net.liftweb.http.SHtml
+import net.liftweb.http.Templates
+import net.liftweb.http.js.JsCmd
+import net.liftweb.http.js.JsCmds._
+import net.liftweb.http.js.JE._
+import net.liftweb.http.js.jquery.JqJsCmds._
+
+import java.text.SimpleDateFormat
+
+class ContactTable
+{
+    val currentUser = CurrentUser.is.get
+    def contacts = Contact.findByUser(currentUser).openOr(Nil)
+
+    def createContactRow(contact: Contact) = {
+        ".name *" #> contact.name.is &
+        ".email *" #> contact.email.is &
+        ".phone *" #> contact.phone.is &
+        ".address *" #> contact.address.is
+    }
+
+    def render = {
+        ".contactRow" #> contacts.map(createContactRow)
+    }
+}
