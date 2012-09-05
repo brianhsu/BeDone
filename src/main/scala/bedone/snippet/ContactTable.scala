837823c27a90c07ec3f2db162c06bb04ae5638c0
Brian Hsu <brianhsu.hsu@gmail.com>
2012-09-05 12:06:24 +0800

    Implement delete of contacts.

--- src/main/scala/bedone/snippet/ContactTable.scala
+++ src/main/scala/bedone/snippet/ContactTable.scala
@@ -14,17 +14,23 @@ import net.liftweb.http.js.jquery.JqJsCmds._
 
 import java.text.SimpleDateFormat
 
-class ContactTable
+class ContactTable extends JSImplicit
 {
     val currentUser = CurrentUser.is.get
     def contacts = Contact.findByUser(currentUser).openOr(Nil).filterNot(_.isTrash.is)
 
+    def deleteContact(contact: Contact)(value: String) = {
+        contact.isTrash(true).saveTheRecord()
+        FadeOutAndRemove("contact" + contact.idField.is)
+    }
+
     def createContactRow(contact: Contact) = {
         ".contactRow [id]" #> ("contact" + contact.idField.is) &
         ".name *" #> contact.name.is &
-        ".email *" #> contact.email.is &
-        ".phone *" #> contact.phone.is &
-        ".address *" #> contact.address.is
+        ".email *" #> contact.email.is.getOrElse("") &
+        ".phone *" #> contact.phone.is.getOrElse("") &
+        ".address *" #> contact.address.is.getOrElse("") &
+        ".delete [onclick]" #> SHtml.onEvent(deleteContact(contact))
     }
 
     def render = {
