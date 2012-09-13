6e8669d1ffd20c760f33d18ec23d09e93d44fc7a
Brian Hsu <brianhsu.hsu@gmail.com>
2012-09-13 14:38:01 +0800

    Add basic edit HTML layout.

--- src/main/scala/bedone/snippet/ContactTable.scala
+++ src/main/scala/bedone/snippet/ContactTable.scala
@@ -15,22 +15,6 @@ import net.liftweb.http.js.jquery.JqJsCmds._
 
 import java.text.SimpleDateFormat
 
-class ContactDetail(contact: Contact) extends JSImplicit
-{
-    def render = {
-        ".name *"       #> contact.name.is &
-        ".email *"      #> contact.email.is.getOrElse("") &
-        ".email [href]" #> contact.email.is.map("mailto:" + _).getOrElse("#") &
-        ".phone *"      #> contact.phone.is.getOrElse("") &
-        ".phone [href]" #> contact.phone.is.map("tel:" + _).getOrElse("#") &
-        ".address *"    #> contact.address.is.getOrElse("") &
-        ".avatar [src]" #> (Gravatar.avatarURL(contact.email.is.getOrElse("")) + "?d=mm") &
-        "#delegatedActionArea [data-lift]" #> (
-            "lift:embed?what=delegated/list?contactID=" + contact.idField.is
-        )
-    }
-}
-
 class ContactTable extends JSImplicit
 {
     val currentUser = CurrentUser.is.get
