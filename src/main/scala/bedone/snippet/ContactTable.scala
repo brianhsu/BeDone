fa106cab4223755e05dbbc543e956030e762bd3c
Brian Hsu <brianhsu.hsu@gmail.com>
2012-09-20 17:02:40 +0800

    Implement batch delete contacts.

--- src/main/scala/bedone/snippet/ContactTable.scala
+++ src/main/scala/bedone/snippet/ContactTable.scala
@@ -24,6 +24,8 @@ class ContactTable extends JSImplicit
     val currentUser = CurrentUser.is.get
     def contacts = Contact.findByUser(currentUser).openOr(Nil).filterNot(_.isTrash.is)
 
+    var selected: Set[Contact] = Set()
+
     def editContact(contact: Contact): JsCmd = {
 
         def callback(contact: Contact) = {
@@ -44,10 +46,30 @@ class ContactTable extends JSImplicit
         FadeOutAndRemove("contact" + contact.idField.is)
     }
 
+    def select(contact: Contact)(checked: Boolean): JsCmd = {
+        this.selected = checked match {
+            case true  => selected + contact
+            case false => selected - contact
+        }
+
+        val updateDeleteButton = this.selected.size match {
+            case 0 => """$('#deleteSelected').hide()"""
+            case _ => """$('#deleteSelected').show()"""
+        }
+
+        val updateSelectAll = (this.selected.size == this.contacts.size) match {
+            case true  => """$('#selectAll').attr('checked', true)"""
+            case false => """$('#selectAll').attr('checked', false)"""
+        }
+
+        updateDeleteButton & updateSelectAll
+    }
+
     def createContactRow(contact: Contact) = {
         
         val cssBinding = 
             ".contactRow [id]"  #> ("contact" + contact.idField.is) &
+            ".select"           #> SHtml.ajaxCheckbox(false, select(contact)) &
             ".name *"           #> contact.name.is &
             ".email *"          #> contact.email.is.getOrElse("") &
             ".email [href]"     #> contact.email.is.map("mailto:" + _).getOrElse("#") &
@@ -66,8 +88,45 @@ class ContactTable extends JSImplicit
         S.redirectTo(contactsOAuth.authURL)
     }
 
+    def toggleSelectAll(isChecked: Boolean): JsCmd = {
+
+        this.selected = isChecked match {
+            case true  => contacts.toSet
+            case false => Set()
+        }
+
+        val showOrHide = isChecked match {
+            case true => """$('#deleteSelected').show()"""
+            case false => """$('#deleteSelected').hide()"""
+        }
+
+
+        showOrHide &
+        """
+            $('.select').each(function() {
+                $(this).attr('checked', %s);
+            });
+        """.format(isChecked)
+
+    }
+
+    def deleteSelected(): JsCmd = {
+
+        val deleteJS = selected.map { contact => 
+            contact.isTrash(true).saveTheRecord()
+            FadeOutAndRemove("contact" + contact.idField.is)
+        }
+
+        """
+          $('#selectAll').attr('checked', false);
+          $('#deleteSelected').hide();
+        """ & deleteJS.toList
+    }
+
     def render = {
-        "#importContact [onclick]" #> SHtml.onEvent(s => gmailAuth()) &
+        "#selectAll" #> SHtml.ajaxCheckbox(false, toggleSelectAll _) &
+        "#importContact [onclick]"  #> SHtml.onEvent(s => gmailAuth()) &
+        "#deleteSelected [onclick]" #> SHtml.onEvent(s => deleteSelected()) &
         ".contactRow" #> contacts.map(createContactRow)
     }
 }
