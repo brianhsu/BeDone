ee4d40048fc967235c2a34cceb41aaf042054b68
Brian Hsu <brianhsu.hsu@gmail.com>
2012-11-08 16:30:51 +0800

    Add summary snippet.

--- src/main/scala/bedone/snippet/Dashboard.scala
+++ src/main/scala/bedone/snippet/Dashboard.scala
@@ -1,20 +1,44 @@
 package org.bedone.snippet
 
-import net.liftweb.common.Box
-import net.liftweb.common.Empty
-import net.liftweb.common.Full
-
+import org.bedone.model._
 import net.liftweb.util.Helpers._
-import net.liftweb.http.S
-import net.liftweb.http.SessionVar
 
-import org.scribe.builder._
-import org.scribe.builder.api._
-import org.scribe.model._
 
 class DashBoard
 {
+    val currentUser = CurrentUser.get.get
+
+    private val actions = Action.findByUser(currentUser).openOr(Nil)
+    private val totalActions = actions.size
+    private val doneActions = actions.filter(_.action.isDone.is).size
+    private val doneActionsPercent = totalActions match {
+        case 0     => 0
+        case total => (doneActions.toDouble / total) * 100
+    }
+
+    private val delegateds = Delegated.findByUser(currentUser).openOr(Nil)
+    private val totalDelegated = delegateds.size
+    private val doneDelegated = delegateds.filter(_.action.isDone.is).size
+    private val doneDelegatedPercent = totalDelegated match {
+        case 0     => 0
+        case total => (doneDelegated.toDouble / total) * 100
+    }
+
+    private val scheduleds = Scheduled.findByUser(currentUser).openOr(Nil)
+    private val totalScheduled = scheduleds.size
+    private val doneScheduled = scheduleds.filter(_.action.isDone.is).size
+    private val doneScheduledPercent = totalScheduled match {
+        case 0     => 0
+        case total => (doneScheduled.toDouble / total) * 100
+    }
+
     def render = {
-        "aaa" #> "QQQ"
+        "#nextActionProgress *" #> "%d / %d".format(doneActions, totalActions) &
+        "#nextActionBar [style]" #> ("width: " + doneActionsPercent + "%") &
+        "#delegatedProgress *" #> "%d / %d".format(doneDelegated, totalDelegated) &
+        "#delegatedBar [style]" #> ("width: " + doneDelegatedPercent + "%") &
+        "#scheduledProgress *" #> "%d / %d".format(totalScheduled, doneScheduled) &
+        "#scheduledBar [style]" #> ("width: " + doneScheduledPercent + "%")
+
     }
 }
